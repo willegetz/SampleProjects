1fac28ad7cf8df91c09d3b3303f59d31450baee2
William Getz <willegetz@gmail.com>
2017-08-15 14:52:17 -0700

    Moved shipyard into nauticalvessels folder and updated references

---
+++ NauticalVessels/ShipYard/Facilities/Construction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipYard
+{
+    public class Construction
+    {
+        public TallShip GetClipper()
+        {
+            return new TallShip()
+            {
+                Name = "Clipper Ship",
+                Masts = "3",
+                Space = "200 MO"
+            };
+        }
+    }
+}
